9ca3ebda46af4be4c3b1f8cb2ccd726cf2191170
JamshidXamroyev <frontend.dev0811@gmail.com>
2025-01-16 14:07:31 +0500

    Lists bo'limi qo'shildi!

--- admin-panel/src/pages/add.jsx
+++ admin-panel/src/pages/add.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {assets} from '../admin_assets/assets'
 import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Add = () => {
   const [image, setImage] = useState(false)
@@ -12,8 +13,8 @@ const Add = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
-    setLaod(true);  // Yangi yuklash holatini boshlash
- 
+    setLaod(true);  
+
     const formData = new FormData();
     formData.append('name', name);
     formData.append('description', description);
@@ -21,71 +22,66 @@ const Add = () => {
     formData.append('category', category);
     formData.append('image', image);  // 'imageFile' fayl inputidagi fayl
 
-    try {
-      console.log(formData);
-        const response = await fetch('/add', {
-            method: 'POST',
-            body: formData  // Ma'lumotlarni formData formatida yuborish
-        });
-
-        const data = await response.json();
-
-        if (data.error) {
-            throw new Error(data.error);  // Xatolikni tashlash
-        } else {
-            console.log(data);  // Yuborilgan ma'lumot
-            toast.success('Food added!');
-        }
-    } catch (error) {
-        console.log(error);
-        toast.error('Failed to add food!');
-    } 
+    const response = await axios.post('/add', formData)
+    if (response.status !== 200 && response.data.error) {
+        throw new Error(response.data.error);  // Xatolikni tashlash
+        toast.error("Food didn't added")
+    } else {
+        toast.success('Food added!');
+    }
+    setLaod(false)
 };
   return ( 
     <section className="md:w-1/2 w-full mb-6 md:px-7 px-2">
       {/* <h2 className='text-center text-4xl font-bold'>Add a new Food</h2> */}
-      <form onSubmit={onSubmitHandler}>
-        <div className="flex flex-col gap-2 mt-6">
-          <p>Upload image</p>
-          <label htmlFor="image" className='w-[180px]'>
-            <img src={image ? URL.createObjectURL(image) : assets.upload_area} alt="imagee" />
-          </label>
-          <input type="file" id='image' onChange={(e) => setImage(e.target.files[0]) } hidden/>
+      {load ? (
+        <div className='w-full h-[80vh] flex justify-center items-center'>
+          <div className='spinner'></div>
         </div>
-
-        <div className='md:w-1/2 w-full my-4 flex flex-col'>
-          <p>Food name</p>
-          <input type="text" onChange={(e) => setName(e.target.value)} value={name} name='name' className='w-full py-2 border px-3 rounded-sm focus:border-black outline-none' placeholder='Enter food name...'/>
-        </div>
-
-        <div className='my-4 md:w-1/2 w-full'>
-          <p>Food description</p>
-          <textarea name="description" rows={6} onChange={(e) => setDescription(e.target.value)} value={description} className='w-full border focus:border-black p-2 outline-none' placeholder='Enter description here'></textarea>
-        </div>
-
-        <div className='flex justify-between items-center gap-5 md:w-1/2 w-full'>
-          <div className='w-1/2'>
-            <p>Product category</p>
-            <select name='category' onChange={(e) => setCategory(e.target.value)} value={category} className='w-full border my-2 p-1 outline-none focus:border-black'>
-              <option value="Salad">Salad</option>
-              <option value="Rolls">Rolls</option>
-              <option value="Deserts">Deserts</option>
-              <option value="Sandwich">Sandwich</option>
-              <option value="Cake">Cake</option>
-              <option value="Pure Veg">Pure Veg</option>
-              <option value="Pasta">Pasta</option>
-              <option value="Noodles">Noodles</option>
-              {load}
-            </select>
+      ) : (
+        <form onSubmit={onSubmitHandler}>
+          <div className="flex flex-col gap-2 mt-6">
+            <p>Upload image</p>
+            <label htmlFor="image" className='w-[180px]'>
+              <img src={image ? URL.createObjectURL(image) : assets.upload_area} alt="imagee" />
+            </label>
+            <input type="file" id='image' onChange={(e) => setImage(e.target.files[0]) } hidden/>
           </div>
 
-          <div className='w-1/2'>
-            <p>Product Price</p>
-            <input type="number" name='price' onChange={(e) => setPrice(e.target.value)} value={price} placeholder='$20' className='w-full p-1 my-2 outline-none border focus:border-black'/>
+          <div className='md:w-1/2 w-full my-4 flex flex-col'>
+            <p>Food name</p>
+            <input type="text" onChange={(e) => setName(e.target.value)} value={name} name='name' className='w-full py-2 border px-3 rounded-sm focus:border-black outline-none' placeholder='Enter food name...'/>
           </div>
-        </div>
-        <button type='submit' className='md:w-1/2 w-full my-2 border py-1 border-orange-700 bg-orange-600 text-white font-semibold active:bg-orange-700 rounded-sm'>ADD</button>
-      </form>
+
+          <div className='my-4 md:w-1/2 w-full'>
+            <p>Food description</p>
+            <textarea name="description" rows={6} onChange={(e) => setDescription(e.target.value)} value={description} className='w-full border focus:border-black p-2 outline-none' placeholder='Enter description here'></textarea>
+          </div>
+
+          <div className='flex justify-between items-center gap-5 md:w-1/2 w-full'>
+            <div className='w-1/2'>
+              <p>Product category</p>
+              <select name='category' onChange={(e) => setCategory(e.target.value)} value={category} className='w-full border my-2 p-1 outline-none focus:border-black'>
+                <option value="Salad">Salad</option>
+                <option value="Rolls">Rolls</option>
+                <option value="Deserts">Deserts</option>
+                <option value="Sandwich">Sandwich</option>
+                <option value="Cake">Cake</option>
+                <option value="Pure Veg">Pure Veg</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Noodles">Noodles</option>
+                {load}
+              </select>
+            </div>
+
+            <div className='w-1/2'>
+              <p>Product Price</p>
+              <input type="number" name='price' onChange={(e) => setPrice(e.target.value)} value={price} placeholder='$20' className='w-full p-1 my-2 outline-none border focus:border-black'/>
+            </div>
+          </div>
+          <button type='submit' className='md:w-1/2 w-full my-2 border py-1 border-orange-700 bg-orange-600 text-white font-semibold active:bg-orange-700 rounded-sm'>ADD</button>
+        </form>
+      )}
     </section>
   )
 }
--- admin-panel/src/pages/list.jsx
+++ admin-panel/src/pages/list.jsx
@@ -1,6 +1,64 @@
+import { useEffect, useState } from "react"
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+
 const List = () => {
+  const [list, setList] = useState([])
+  const [load, setLoad] = useState(false)
+  const navigate = useNavigate()
+
+  const fetchList = async() => {
+    setLoad(true)
+    const response = await axios.get("/list")
+    setList(response.data)
+    setLoad(false)
+  }
+
+  const deleteHandler = async id => {
+    setLoad(true)
+    await axios.delete(`/remove/${id}`)
+    setLoad(false)
+    navigate("/")
+  }
+  
+  useEffect(() => {
+    fetchList()
+  }, [])
+
   return (
-    <div>List</div>
+    <section className="w-full md:px-4">
+      <h2 className="my-2">All Food Lists</h2>
+      <div className="flex flex-col w-full">
+        <div className="grid grid-cols-5 items-center gap-2 max-sm:text-[12px] border-t bg-gray-200 p-2">
+          <b>Images</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {load ? (
+          <div className="w-full h-[10vh] flex justify-center items-center">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <div>
+              {list.length !== 0 ? list.map((item, index) => (
+              <div className="grid grid-cols-5 items-center gap-2 max-sm:text-[12px] border-y p-2 border-black">
+                <img src={`http://localhost:2008/images/${item.image}`} alt="Images" className="w-[90px]"/>
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>{item.price}$</p>
+                <p className="cursor-pointer text-xl select-none" onClick={() => deleteHandler(item._id)}>x</p>
+              </div>
+            )) : (
+              <div className="text-center">
+                <h1 className="font-bold font-title my-2 md:text-4xl text-2xl"><span className="text-orange-800">No</span> Any Food</h1>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
